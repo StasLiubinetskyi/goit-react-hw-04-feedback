4a9a5c55fb5bd1d191ba50ffc3e4850d61d54fe6
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-08-02 22:26:34 +0300

    3 useState

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,29 +4,32 @@ import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-const initialFeedbackState = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
-
 const App = () => {
-  const [feedback, setFeedback] = useState(initialFeedbackState);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleLeaveFeedback = option => {
-    setFeedback(prevState => ({
-      ...prevState,
-      [option]: prevState[option] + 1,
-    }));
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
   const countTotalFeedback = () => {
-    const { good, neutral, bad } = feedback;
     return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    const { good } = feedback;
     const total = countTotalFeedback();
     return total > 0 ? Math.round((good / total) * 100) : 0;
   };
@@ -46,9 +49,9 @@ const App = () => {
       <Section title="Statistics">
         {hasFeedback ? (
           <Statistics
-            good={feedback.good}
-            neutral={feedback.neutral}
-            bad={feedback.bad}
+            good={good}
+            neutral={neutral}
+            bad={bad}
             total={totalFeedback}
             positivePercentage={positivePercentage}
           />
